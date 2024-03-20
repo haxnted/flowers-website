58610791bc8745ad3ca5e3728a165462de71927f
alex bagnencko <bagnencko.sasha1@gmail.com>
2024-03-20 19:39:04 +0300

    finished structure landing

---
+++ js/index.js
@@ -0,0 +1,16 @@
+var list1 = document.getElementById('list1')
+var list2 = document.getElementById('list2')
+var list3 = document.getElementById('list3')
+
+function ShowList1() {
+	if (list1.style.display === 'none') list1.style.display = 'block'
+	else list1.style.display = 'none'
+}
+function ShowList2() {
+	if (list2.style.display === 'none') list2.style.display = 'block'
+	else list2.style.display = 'none'
+}
+function ShowList3() {
+	if (list3.style.display === 'none') list3.style.display = 'block'
+	else list3.style.display = 'none'
+}
