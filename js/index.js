8a42c946f83ab79f2417d8eab1455783d7371f2a
alex bagnencko <bagnencko.sasha1@gmail.com>
2024-03-21 19:50:35 +0300

    finish fron-end

--- js/index.js
+++ js/index.js
@@ -1,16 +1,25 @@
 var list1 = document.getElementById('list1')
 var list2 = document.getElementById('list2')
 var list3 = document.getElementById('list3')
+var item = document.getElementById('pc-submenu')
+
+function toggleDisplay(element) {
+	if (element.style.display === 'none') element.style.display = 'block'
+	else element.style.display = 'none'
+}
 
 function ShowList1() {
-	if (list1.style.display === 'none') list1.style.display = 'block'
-	else list1.style.display = 'none'
+	toggleDisplay(list1)
 }
+
 function ShowList2() {
-	if (list2.style.display === 'none') list2.style.display = 'block'
-	else list2.style.display = 'none'
+	toggleDisplay(list2)
 }
+
 function ShowList3() {
-	if (list3.style.display === 'none') list3.style.display = 'block'
-	else list3.style.display = 'none'
+	toggleDisplay(list3)
+}
+
+function ShowHeaderMenu() {
+	toggleDisplay(item)
 }
